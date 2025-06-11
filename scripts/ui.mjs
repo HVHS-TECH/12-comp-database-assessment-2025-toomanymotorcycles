9ec75710fda3903d90c802e827fe4b17db7b5f75
Mr Explosive <106851434+toomanymotorcycles@users.noreply.github.com>
2025-06-11 22:00:02 +0000

    modified:   games.html 	modified:   index.html 	new file:   load-32_128.gif 	modified:   scripts/ui.mjs 	modified:   style.css

--- scripts/ui.mjs
+++ scripts/ui.mjs
@@ -1,9 +1,6 @@
 export {openLightbox, closeLightboxes}
 
 function openLightbox(id) {
-            for (var i=1; i<document.getElementsByClassName("lightbox").length; i++) {
-                document.getElementsByClassName("lightbox")[i].style.display = "none";
-            }
             document.getElementById("authLightboxes").style.display = "block";
             document.getElementById(id).style.display = "block";
         }
