bde23f43da823485db3081b462b8db48dfec4329
Mr Explosive <106851434+toomanymotorcycles@users.noreply.github.com>
2025-06-25 07:13:37 +0000

    modified:   games.html 	modified:   scripts/ui.mjs 	modified:   style.css

--- scripts/ui.mjs
+++ scripts/ui.mjs
@@ -15,7 +15,7 @@ function openGameLightbox(title, desc, playURL, imgURL) {
         }
 
 function closeLightboxes() {
-    for (var i=1; i<document.getElementsByClassName("lightbox").length; i++) {
+    for (var i=0; i<document.getElementsByClassName("lightbox").length; i++) {
         document.getElementsByClassName("lightbox")[i].style.display = "none";
     }
     document.getElementById("authLightboxes").style.display = "none";
